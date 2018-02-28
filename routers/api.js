6a51f2cb06924c9bc02f2422eb2213ac65ef3a0d
guoxiaojiang <guoxiaojiang@chehejia.com>
2018-02-28 11:04:15 +0800

    add verify test

--- routers/api.js
+++ routers/api.js
@@ -13,6 +13,12 @@ var qs=require('querystring');//解析参数的库
 
 var mongodb = require('mongodb').MongoClient;
 
+var QcloudSms = require("../qcloudsms_js");
+var appid = 1400070292;
+var appkey = "d749d35ca6d1964d99827e0bc871c4d2";
+var templId = 89987;
+var qcloudsms = QcloudSms(appid, appkey);
+
 
 router.use(function (req, res, next) {
 
@@ -85,6 +91,14 @@ router.get('/truckDetail', function (req, res) {
     res.json(responseData);
 })
 
+function callback(err, res, resData) {
+    if (err)
+        console.log("err: ", err);
+    else
+        console.log("response data: ", resData);
+}
+
+
 router.get('/verify', function (req, res) {
     var arg=url.parse(req.url).query;
     var phone = qs.parse(arg)['phone'];
@@ -92,8 +106,11 @@ router.get('/verify', function (req, res) {
     for(var i=0;i<4;i++){
         code+=Math.floor(Math.random()*10)
     }
-    console.log("suiji:"+code);
+    console.log("random code:"+code);
     //给phone发送随机验证码
+    var ssender = qcloudsms.SmsSingleSender();
+    var params = [code];
+    ssender.sendWithParam(86, phone, templId, params, "", "", "", callback);
     responseData.vcode = code;
     responseData.message = 'ok';
     res.json(responseData);
